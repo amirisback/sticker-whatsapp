e30da380b305b377e6927eed34bdd10f6ac1a618
Muhammad Faisal Amir <faisalamircs@gmail.com>
2021-07-03 10:21:07 +0700

    fixing : issues #2 update to latest fresco version

--- app/src/main/java/com/frogobox/stickerwa/base/BaseActivity.kt
+++ app/src/main/java/com/frogobox/stickerwa/base/BaseActivity.kt
@@ -27,7 +27,8 @@ abstract class BaseActivity : FrogoAdmobActivity() {
         setBasePublisherID(getString(R.string.admob_publisher_id))
         setBaseBannerAdUnitID(getString(R.string.admob_banner))
         setBaseInterstialAdUnitID(getString(R.string.admob_interstitial))
-        setBaseRewardedAdUnitID(getString(R.string.admob_rewarded_video))
+        setBaseRewardedAdUnitID(getString(R.string.admob_rewarded))
+        setBaseRewardedInterstitialAdUnitID(getString(R.string.admob_rewarded_interstitial))
         setBaseAdmob()
     }
 
